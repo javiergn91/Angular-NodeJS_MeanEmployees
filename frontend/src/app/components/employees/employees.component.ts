ceb689749acb97eb2e8d6b01fed212420ee0fec5
Javier <javier.gonzalez@ausmastudios.com>
2018-09-23 08:36:19 -0300

    Resto de funcionalidades

--- frontend/src/app/components/employees/employees.component.ts
+++ frontend/src/app/components/employees/employees.component.ts
@@ -3,6 +3,8 @@ import { EmployeeService } from "../../services/employee.service";
 import { NgForm } from '@angular/forms/src/directives/ng_form';
 import { Employee } from "../../models/employee";
 
+declare var M: any;
+
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
@@ -14,12 +16,24 @@ export class EmployeesComponent implements OnInit {
   constructor(private employeeService: EmployeeService) { }
 
   ngOnInit() {
+    this.getEmployees();
   }
 
   addEmployee(form:NgForm) {
-    this.employeeService.postEmployee(form.value).subscribe(res => {
-      this.resetForm(form);
-    });
+    if(form.value._id) {
+      this.employeeService.putEmployee(form.value)
+        .subscribe(res => {
+          this.resetForm();
+          M.toast({html: "Updated successfully"});
+          this.getEmployees();
+        });
+    } else {
+      this.employeeService.postEmployee(form.value).subscribe(res => {
+        this.resetForm(form);
+        M.toast({html: "Saved successfully"});
+        this.getEmployees();
+      });      
+    }
   }
 
   resetForm(form?:NgForm) {
@@ -27,5 +41,23 @@ export class EmployeesComponent implements OnInit {
       form.reset();
       this.employeeService.selectedEmployee = new Employee();
     }
+  }
+
+  getEmployees() {
+    this.employeeService.getEmployees().subscribe(res => {
+      this.employeeService.employees = res as Employee[];
+    });
+  }
+
+  editEmployee(employee: Employee) {
+    this.employeeService.selectedEmployee = employee;
+  }
+
+  deleteEmployee(_id: string) {
+    this.employeeService.deleteEmployee(_id)
+      .subscribe(res => {
+        M.toast({html: "Deleted successfully"});
+        this.getEmployees();
+      });
   }
 }
--- server/controllers/employee.controller.js
+++ server/controllers/employee.controller.js
@@ -8,7 +8,12 @@ employeeController.getEmployees = async (req, res) => {
 };
 
 employeeController.createEmployee = async (req, res) => {
-    const employee = new Employee(req.body);
+    const employee = new Employee({
+        name: req.body.name,
+        position: req.body.position,
+        office: req.body.office,
+        salary: req.body.salary       
+    });
     await employee.save();
 
     res.json({
